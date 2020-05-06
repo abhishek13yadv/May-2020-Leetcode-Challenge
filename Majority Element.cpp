35eeb06a2a6ff24931b66f65aa9b4eded304a51a
abhishek13yadv <36642139+abhishek13yadv@users.noreply.github.com>
2020-05-06 14:41:25 +0530

    Create Majority Element.cpp

---
+++ Majority Element.cpp
@@ -0,0 +1,11 @@
+Using hashmap
+
+int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> counter;
+        for(int num : nums){
+            if(++counter[num] > nums.size()/2){
+                 return num;
+            }
+        }
+        return 0;
+    }
